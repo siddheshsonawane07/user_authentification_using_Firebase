6aaa871917cb8fba653692b2cc4bb7543f8f4720
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2021-11-29 23:23:30 +0530

    deleted web files

--- lib/pages/welcome/components/background.dart
+++ lib/pages/welcome/components/background.dart
@@ -13,6 +13,7 @@ class Background extends StatelessWidget {
     return SizedBox(
       height: size.height,
       width: double.infinity,
+      // Here i can use size.width but use double.infinity because both work as a same
       child: Stack(
         alignment: Alignment.center,
         children: <Widget>[
@@ -20,8 +21,8 @@ class Background extends StatelessWidget {
             top: 0,
             left: 0,
             child: Image.asset(
-              "assets/images/main_top.png",
-              width: size.width * 0.3,
+              "assets/images/signup_top.png",
+              width: size.width * 0.35,
             ),
           ),
           Positioned(
@@ -29,7 +30,7 @@ class Background extends StatelessWidget {
             left: 0,
             child: Image.asset(
               "assets/images/main_bottom.png",
-              width: size.width * 0.2,
+              width: size.width * 0.25,
             ),
           ),
           child,
