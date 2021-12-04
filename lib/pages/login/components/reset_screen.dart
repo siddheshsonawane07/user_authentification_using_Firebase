688715fe6fb74a83dc5c330a540da257ca176544
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2021-12-04 14:22:35 +0530

    reset_screeen.dart

---
+++ lib/pages/login/components/reset_screen.dart
@@ -0,0 +1,249 @@
+// ignore_for_file: avoid_print, unnecessary_new, duplicate_ignore
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:toycathon/constants.dart';
+import 'package:toycathon/pages/login/components/background.dart';
+import 'package:toycathon/pages/main/dashboard.dart';
+import 'package:toycathon/pages/signup/signup_screen.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+}
+
+class ResetScreen extends StatefulWidget {
+  const ResetScreen({Key? key}) : super(key: key);
+
+  @override
+  _ResetScreen createState() => _ResetScreen();
+}
+
+class _ResetScreen extends State<ResetScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController emailController = new TextEditingController();
+  // ignore: unnecessary_new
+  final TextEditingController passwordController = new TextEditingController();
+  final _auth = FirebaseAuth.instance;
+  String? errorMessage;
+
+  @override
+  Widget build(BuildContext context) {
+    final emailField = TextFormField(
+      autofocus: false,
+      controller: emailController,
+      keyboardType: TextInputType.emailAddress,
+      validator: (value) {
+        if (value!.isEmpty) {
+          return ("Please Enter Your Email");
+        }
+        // reg expression for email validation
+        if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]").hasMatch(value)) {
+          return ("Please Enter a valid email");
+        }
+        return null;
+      },
+      onSaved: (value) {
+        emailController.text = value!;
+      },
+      cursorColor: kPrimaryColor,
+      decoration: const InputDecoration(
+        icon: Icon(
+          Icons.email,
+          color: kPrimaryColor,
+        ),
+        hintText: "Your Email",
+        border: InputBorder.none,
+      ),
+    );
+
+    final passwordField = TextFormField(
+      autofocus: false,
+      controller: passwordController,
+      obscureText: true,
+      validator: (value) {
+        // ignore: unnecessary_new
+        RegExp regex = new RegExp(r'^.{6,}$');
+        if (value!.isEmpty) {
+          return ("Password is required for login");
+        }
+        if (!regex.hasMatch(value)) {
+          return ("Enter Valid Password(Min. 6 Character)");
+        }
+      },
+      onSaved: (value) {
+        passwordController.text = value!;
+      },
+      textInputAction: TextInputAction.done,
+      cursorColor: kPrimaryColor,
+      decoration: const InputDecoration(
+        icon: Icon(
+          Icons.lock,
+          color: kPrimaryColor,
+        ),
+        hintText: "Password",
+        border: InputBorder.none,
+      ),
+    );
+
+    final loginButton = ClipRRect(
+      borderRadius: BorderRadius.circular(25),
+      child: ElevatedButton(
+        onPressed: () {
+          signIn(emailController.text, passwordController.text);
+        },
+        child: const Text(
+          "LOGIN",
+          style: TextStyle(
+              color: kPrimaryLightColor,
+              fontSize: 20,
+              fontWeight: FontWeight.bold),
+        ),
+        style: ElevatedButton.styleFrom(
+          primary: kPrimaryColor,
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          textStyle: const TextStyle(
+              color: kPrimaryLightColor,
+              fontSize: 25,
+              fontWeight: FontWeight.w500),
+        ),
+      ),
+    );
+
+    final ForgotPassword = TextButton(
+      onPressed: () {Navigator.of(context).push(MaterialPageRoute(builder: (context) =>  ResetScreen()));},
+      child: const Text(
+        "Forgot Password ? ",
+        style: TextStyle(
+            color: kPrimaryColor,
+            fontSize: 16,
+            fontWeight: FontWeight.bold),
+      ),
+    );
+
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Background(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(height: size.height * 0.03),
+              const Text(
+                "LOGIN",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+              SizedBox(height: size.height * 0.03),
+              SvgPicture.asset(
+                "assets/icons/login.svg",
+                height: size.height * 0.35,
+              ),
+              SizedBox(height: size.height * 0.03),
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 10),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                width: size.width * 0.9,
+                decoration: BoxDecoration(
+                  color: kPrimaryLightColor,
+                  borderRadius: BorderRadius.circular(29),
+                ),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [emailField, passwordField],
+                  ),
+                ),
+              ),
+              SizedBox(height: size.height * 0.03),
+              loginButton,
+              SizedBox(height: size.height * 0.01),
+              ForgotPassword,
+              SizedBox(height: size.height * 0.02),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(25),
+                child: ElevatedButton(
+                  child: const Text(
+                    "Don't have an account ? SIGNUP",
+                    style: TextStyle(fontSize: 12, color: kPrimaryColor),
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pushReplacement(MaterialPageRoute(
+                        builder: (context) => const SignUpScreen()));
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.white,
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    CheckUserLog();
+  }
+
+  void CheckUserLog() async {
+    final FirebaseAuth auth = FirebaseAuth.instance;
+    final user = await auth.currentUser;
+    if (user != null) {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => const Dashboard()));
+    }
+  }
+
+  void signIn(String email, String password) async {
+    if (_formKey.currentState!.validate()) {
+      try {
+        await _auth
+            .signInWithEmailAndPassword(email: email, password: password)
+            .then((uid) => {
+                  Fluttertoast.showToast(msg: "Login Successful"),
+                  Navigator.of(context).pushReplacement(MaterialPageRoute(
+                      builder: (context) => const Dashboard())),
+                });
+      } on FirebaseAuthException catch (error) {
+        switch (error.code) {
+          case "invalid-email":
+            errorMessage = "Your email address appears to be malformed.";
+
+            break;
+          case "wrong-password":
+            errorMessage = "Your password is wrong.";
+            break;
+          case "user-not-found":
+            errorMessage = "User with this email doesn't exist.";
+            break;
+          case "user-disabled":
+            errorMessage = "User with this email has been disabled.";
+            break;
+          case "too-many-requests":
+            errorMessage = "Too many requests";
+            break;
+          case "operation-not-allowed":
+            errorMessage = "Signing in with Email and Password is not enabled.";
+            break;
+          default:
+            errorMessage = "An undefined Error happened.";
+        }
+        Fluttertoast.showToast(msg: errorMessage!);
+        print(error.code);
+      }
+    }
+  }
+
+
+
+}
